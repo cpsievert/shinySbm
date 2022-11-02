f0c8c1afad8685f7e73e284fbbe4bcc59c4a3e3b
Theodore Vanrenterghem <theodorevanrenterghem@gmail.com>
2022-11-02 10:50:44 +0100

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -48,9 +48,10 @@ usethis::use_git()
 ## Create a template for tests
 golem::use_recommended_tests()
 
+
 ## Favicon ----
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon() # path = "path/to/ico". Can be an online file.
+golem::use_favicon("icon/logo_inrae.png") # path = "path/to/ico". Can be an online file.
 # golem::remove_favicon() # Uncomment to remove the default favicon
 
 ## Add helper functions ----
