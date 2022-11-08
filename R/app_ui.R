8219f70f89384d46333bf0cc44f80b21442940fc
Jo-Theo <theodorevanrenterghem@gmail.com>
2022-11-08 22:34:35 +0100

    i implement thje navbarpage cause' it's prettier

--- R/app_ui.R
+++ R/app_ui.R
@@ -5,92 +5,90 @@
 #' @import shiny
 #' @noRd
 app_ui <- function(request) {
-  fluidPage(
-    # Leave this function for adding external resources
-    golem_add_external_resources(),
-    # Your application UI logic
-    h1("Stochastic Block Model with the sbm package"),
-    mainPanel(
-      tabsetPanel(
-        id = 'main_tab',
-        type = 'pills',
-        tabPanel("Data uploading", value = 'tab_upload',
-                 #IMPORTATION
-                 # Probleme : dev les moyens de lecture du tableau comme dans blockmodelingGUI
-                 # introduction des covariables
-                 sidebarLayout(
-                   sidebarPanel(width = 5,
-                     radioButtons("whichData", "Which data do you want to use ?",
-                                  choices = list("My own data" = "importData",
-                                                 "SBM exemple" = "sbmData"),
-                                  inline = T, selected = character(0)),
-                     conditionalPanel(
-                       condition = "input.whichData == 'sbmData'",
-                       radioButtons("dataBase", "Which network ?",
-                                    choices = list("Fungus & Trees" = "fungus_tree",
-                                                   "Trees & Trees" = "tree_tree"),
-                                    selected = character(0))
-                     ),
-                     conditionalPanel(
-                       condition = "input.whichData == 'importData'",
-                       fileInput("dataFile", label = "Choose the file containing your adjency matrix",
-                                 buttonLabel = "Browse...",
-                                 placeholder = "No file selected",
-                                 multiple = F,
-                                 accept = c("text/plain", ".csv",".tab","xls","xlsx"))
-                     )
-                   ),
+  navbarPage(title = "ShinySBM",
+             id = 'main_tab',
+             golem_add_external_resources(),
+             tabPanel("Data uploading", value = 'tab_upload',
+                      #IMPORTATION
+                      # Probleme : dev les moyens de lecture du tableau comme dans blockmodelingGUI
+                      # introduction des covariables
+                      sidebarLayout(
+                        sidebarPanel(width = 3,
+                                     radioButtons("whichData", "Which data do you want to use ?",
+                                                  choices = list("My own data" = "importData",
+                                                                 "SBM exemple" = "sbmData"),
+                                                  inline = T, selected = "importData"),
+                                     conditionalPanel(
+                                       condition = "input.whichData == 'sbmData'",
+                                       radioButtons("dataBase", "Which network ?",
+                                                    choices = list("Fungus & Trees" = "fungus_tree",
+                                                                   "Trees & Trees" = "tree_tree"),
+                                                    selected = character(0))
+                                     ),
+                                     conditionalPanel(
+                                       condition = "input.whichData == 'importData'",
+                                       fileInput("dataFile", label = "Choose the file containing your adjency matrix",
+                                                 buttonLabel = "Browse...",
+                                                 placeholder = "No file selected",
+                                                 multiple = F,
+                                                 accept = c("text/plain", ".csv",".tab","xls","xlsx"))
+                                     )
+                        ),
 
-                   mainPanel(width = 7,
-                     fluidRow(
-                       radioButtons("whichsep", "What kind of separator should I use ?",
-                                    choices = list("tabulation" = "tab",
-                                                   "comma" = "comma",
-                                                   "semi-colon" = "semicolon"),
-                                    inline = T, selected = character(0))
-                     ),
-                     fluidRow(
-                       verbatimTextOutput("summary")
-                     )
-                   )
-                 )
-        ),
+                        mainPanel(width = 9,
+                                  fluidRow(
+                                    radioButtons("whichsep", "What kind of separator should I use ?",
+                                                 choices = list("tabulation" = "|",
+                                                                "coma" = ",",
+                                                                "semicolon" = ";",
+                                                                "Others" = "others")
+                                                 , selected = character(0))
+                                  ),
+                                  fluidRow(
+                                    verbatimTextOutput("summary")
+                                  )
+                        )
+                      )
+             ),
 
-        tabPanel("Raw Data",value = 'tab_show',
-                 # RAW DATA SHOW
-                 sidebarLayout(
-                   sidebarPanel(width = 3,
-                     radioButtons("whichshow", "Type of visualisation",
-                                  choices = list("Print" = "print",
-                                                 "Plot" = "simpleplot",
-                                                 "Plot (nodes names)" = "namedplot"),
-                                  selected = character(0)),
-                     conditionalPanel(
-                       condition = "input.whichshow == 'simpleplot'",
-                       wellPanel(
-                         fluidRow(
-                           textInput("rowLabel",
-                                     label = "Specify the label for nodes in row",
-                                     value = NULL),
-                           textInput("colLabel",
-                                     label = "Specify the label for nodes in col",
-                                     value = NULL),
-                           downloadButton("downloadrawPlot", "Download Plot")
-                         )
-                       )
-                     )
-                   ),
+             tabPanel("Visualisation",value = 'tab_show',
+                      # RAW DATA SHOW
+                      sidebarLayout(
+                        sidebarPanel(width = 3,
+                                     radioButtons("whichshow", "Type of visualisation",
+                                                  choices = list("Print" = "print",
+                                                                 "Plot" = "plot"),
+                                                  selected = 'print'),
+                                     conditionalPanel(
+                                       condition = "input.whichshow == 'plot'",
+                                       wellPanel(
+                                         fluidRow(
+                                           textInput("rowLabel",
+                                                     label = "Specify the label for nodes in row",
+                                                     value = NULL),
+                                           textInput("colLabel",
+                                                     label = "Specify the label for nodes in col",
+                                                     value = NULL),
+                                           downloadButton("downloadrawPlot", "Download Plot")
+                                         )
+                                       )
+                                     )
+                        ),
 
-                   mainPanel(width = 9,
-                     DT::dataTableOutput("matrixprint"),
-                     plotOutput("matrixplot"),
-                     plotOutput("matrixplot2")
-                   )
-                 )
-        )
-      )
-    )
+                        mainPanel(width = 9,
+                                  conditionalPanel(
+                                    condition = "input.whichshow == 'print'",
+                                    DT::dataTableOutput("matrixprint")),
+                                  conditionalPanel(
+                                    condition = "input.whichshow == 'plot'",
+                                    imageOutput("matrixplot"))
+                        )
+                      )
+             )
+
+
   )
+
 }
 
 #' Add external Resources to the Application
