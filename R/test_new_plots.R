2245626c3dab0ab6e7753ca6daeeb68e3aa30cb9
Jo-Theo <theodorevanrenterghem@gmail.com>
2023-02-17 00:41:54 +0100

    deplacé

--- R/test_new_plots.R
+++ R/test_new_plots.R
@@ -60,60 +60,60 @@ plot_sbm <- function(fit,ordered = F, transpose = NULL, labels = list(row = 'row
 
 }
 
-data_bi <- fungusTreeNetwork$fungus_tree
-data_uni <- fungusTreeNetwork$tree_tree
-data_uni2 <- (fungusTreeNetwork$tree_tree<1)*1
-my_sbm_bi <- estimateBipartiteSBM(data_bi)
-my_sbm_uni <- estimateSimpleSBM(data_uni, model = 'poisson')
-my_sbm_uni2 <- estimateSimpleSBM(data_uni2)
-
-fit <- my_sbm_uni
-ord <- T
-transpose <- T
-labels <- list(col = 'tree',row = 'fungus')
-
-
-clustering <- list(row = fit$memberships,col = fit$memberships)
-if(ordered){
-  oRow <- order(clustering$row)
-  oCol <- order(clustering$col)
-  uRow <- cumsum(table(clustering$row)) + 0.5
-  uCol <- cumsum(table(clustering$col)) + 0.5
-  uCol <- uCol[-length(uCol)]
-  uRow <- uRow[-length(uRow)]
-  mat_exp <- fit$connectParam$mean[clustering$row,clustering$col][oRow,oCol]
-  mat_pure <- fit$networkData[oRow,oCol]
-}else{
-  tRow <- NULL
-  tCol <- NULL
-  mat_exp <- fit$connectParam$mean[clustering$row,clustering$col]
-  mat_pure <- fit$networkData
-}
-
-plot_net <- reshape2::melt(mat_exp) %>%
-  mutate(base_value  = reshape2::melt(mat_pure)$value)
-
-
-ggplot(data = plot_net, aes(x = Var2, y = Var1, fill = base_value, alpha = base_value)) +
-  geom_tile(aes(x = Var2, y = Var1, alpha = value), fill = 'red', size = 0, show.legend = FALSE) +
-  geom_tile(show.legend = FALSE) +
-  geom_hline(yintercept = uRow,
-             col = 'red', size = .3) +
-  geom_vline(xintercept = uCol,
-             col = 'red', size = .3) +
-  scale_fill_gradient(low = scales::muted("white"), high = 'black',
-                      guide = "colourbar") +
-  ylab("") + xlab("") +
-  scale_alpha(range = c(0,1)) +
-  scale_x_discrete(breaks = "")+
-  scale_y_discrete(breaks = "",guide = guide_axis(angle = 0)) +
-  coord_equal(expand = FALSE) +
-  theme_bw(base_size = 20, base_rect_size = 1, base_line_size  = 1) +
-  theme(axis.ticks =  element_blank())
-
-
-
-plot_sbm(my_sbm_bi, ordered = T, transpose = T,col_pred = 'deeppink3',col_value = 'darkblue')
-
-
-plot_sbm(my_sbm_uni, ordered = T, transpose = F,col_pred = 'deeppink3',col_value = 'darkblue')
+# data_bi <- fungusTreeNetwork$fungus_tree
+# data_uni <- fungusTreeNetwork$tree_tree
+# data_uni2 <- (fungusTreeNetwork$tree_tree<1)*1
+# my_sbm_bi <- estimateBipartiteSBM(data_bi)
+# my_sbm_uni <- estimateSimpleSBM(data_uni, model = 'poisson')
+# my_sbm_uni2 <- estimateSimpleSBM(data_uni2)
+#
+# fit <- my_sbm_uni
+# ord <- T
+# transpose <- T
+# labels <- list(col = 'tree',row = 'fungus')
+#
+#
+# clustering <- list(row = fit$memberships,col = fit$memberships)
+# if(ordered){
+#   oRow <- order(clustering$row)
+#   oCol <- order(clustering$col)
+#   uRow <- cumsum(table(clustering$row)) + 0.5
+#   uCol <- cumsum(table(clustering$col)) + 0.5
+#   uCol <- uCol[-length(uCol)]
+#   uRow <- uRow[-length(uRow)]
+#   mat_exp <- fit$connectParam$mean[clustering$row,clustering$col][oRow,oCol]
+#   mat_pure <- fit$networkData[oRow,oCol]
+# }else{
+#   tRow <- NULL
+#   tCol <- NULL
+#   mat_exp <- fit$connectParam$mean[clustering$row,clustering$col]
+#   mat_pure <- fit$networkData
+# }
+#
+# plot_net <- reshape2::melt(mat_exp) %>%
+#   mutate(base_value  = reshape2::melt(mat_pure)$value)
+#
+#
+# ggplot(data = plot_net, aes(x = Var2, y = Var1, fill = base_value, alpha = base_value)) +
+#   geom_tile(aes(x = Var2, y = Var1, alpha = value), fill = 'red', size = 0, show.legend = FALSE) +
+#   geom_tile(show.legend = FALSE) +
+#   geom_hline(yintercept = uRow,
+#              col = 'red', size = .3) +
+#   geom_vline(xintercept = uCol,
+#              col = 'red', size = .3) +
+#   scale_fill_gradient(low = scales::muted("white"), high = 'black',
+#                       guide = "colourbar") +
+#   ylab("") + xlab("") +
+#   scale_alpha(range = c(0,1)) +
+#   scale_x_discrete(breaks = "")+
+#   scale_y_discrete(breaks = "",guide = guide_axis(angle = 0)) +
+#   coord_equal(expand = FALSE) +
+#   theme_bw(base_size = 20, base_rect_size = 1, base_line_size  = 1) +
+#   theme(axis.ticks =  element_blank())
+#
+#
+#
+# plot_sbm(my_sbm_bi, ordered = T, transpose = T,col_pred = 'deeppink3',col_value = 'darkblue')
+#
+#
+# plot_sbm(my_sbm_uni, ordered = T, transpose = F,col_pred = 'deeppink3',col_value = 'darkblue')
