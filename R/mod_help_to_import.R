6f87317dbd62a86af42d9578862bdcd5f393f68c
Theodore Vanrenterghem <theodore.vanrenterghem@inrae.fr>
2023-04-20 15:48:47 +0200

    add default values depend where we use it there will be differents parmeter available : 1 - upload : everything but strat empty 2 - sbm : only sbmMartix

--- R/mod_help_to_import.R
+++ R/mod_help_to_import.R
@@ -19,7 +19,7 @@ mod_help_to_import_ui <- function(id) {
 #' help_to_import Server Functions
 #'
 #' @noRd
-mod_help_to_import_server <- function(id, rawData, input_upload) {
+mod_help_to_import_server <- function(id, rawData = NULL, sbmData = NULL, input_upload = NULL) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
@@ -29,16 +29,16 @@ mod_help_to_import_server <- function(id, rawData, input_upload) {
     )
 
     observe({
-      if (input_upload$whichData == "importData") {
+      if(!is.null(rawData) & !is.null(input_upload) && input_upload$whichData == "importData"){
         warns <- list()
         mess <- list()
         withCallingHandlers(check_data_inputs(dta = rawData(), inputs = input_upload),
-          warning = function(w) {
-            warns <<- c(warns, list(w))
-          },
-          message = function(m) {
-            mess <<- c(mess, list(m))
-          }
+                            warning = function(w) {
+                              warns <<- c(warns, list(w))
+                            },
+                            message = function(m) {
+                              mess <<- c(mess, list(m))
+                            }
         )
         warn_list$messages <- sapply(mess, function(mess) mess$message)
         warn_list$warnings <- sapply(warns, function(warn) warn$message)
@@ -48,6 +48,39 @@ mod_help_to_import_server <- function(id, rawData, input_upload) {
       }
     })
 
+
+    observe({
+      if(!is.null(input_upload) & !is.null(sbmData)){
+        warns <- list()
+        mess <- list()
+        withCallingHandlers(check_sbm_inputs(sbmData = sbmData(), inputs = input_upload),
+                            warning = function(w) {
+                              warns <<- c(warns, list(w))
+                            },
+                            message = function(m) {
+                              mess <<- c(mess, list(m))
+                            }
+        )
+        warn_list$messages <- sapply(mess, function(mess) mess$message)
+        warn_list$warnings <- sapply(warns, function(warn) warn$message)
+      }else if(!is.null(sbmData)){
+        warns <- list()
+        mess <- list()
+        withCallingHandlers(is.sbmMatrix(sbmData(), warnings = T),
+                            warning = function(w) {
+                              warns <<- c(warns, list(w))
+                            },
+                            message = function(m) {
+                              mess <<- c(mess, list(m))
+                            }
+        )
+        warn_list$messages <- sapply(mess, function(mess) mess$message)
+        warn_list$warnings <- sapply(warns, function(warn) warn$message)
+      }else{
+        warn_list$messages <- list()
+        warn_list$warnings <- list()
+      }
+    })
 
 
 
