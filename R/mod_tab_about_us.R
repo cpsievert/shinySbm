f4fe3f72e9857121250ec059bd2b65f2708d08bb
Theodore Vanrenterghem <theodore.vanrenterghem@inrae.fr>
2023-06-05 17:42:39 +0200

    no bug on files

--- R/mod_tab_about_us.R
+++ R/mod_tab_about_us.R
@@ -16,19 +16,19 @@ mod_tab_about_us_ui <- function(id) {
       div(HTML("<p>
                   Any questions, problems or comments regarding this application ? <br>
                   <u>Contact us:</u>
-                  <a href = \"mailto:shiny.sbm.dev@gmail.com?subject=[ShinySBM]\">shiny.sbm.dev@gmail.com</a>
+                  <a href = \"mailto:shiny.sbm.dev@gmail.com?subject=[ShinySBM]\" target = \"_blank\">shiny.sbm.dev@gmail.com</a>
                 </p>
 
                 <p>
                   For more information about this tool, you can access to the repository on
-                  <a href = \"https://forgemia.inra.fr/theodore.vanrenterghem/shinySbm\">this site</a>.
+                  <a href = \"https://forgemia.inra.fr/theodore.vanrenterghem/shinySbm\" target = \"_blank\">this site</a>.
                 </p>
 
                 <p>
                   <u>Cite us:</u> Research teams that have used ShinySBM are expected to : <br>
                   <ul>
                     <li>Cite the <b>sbm</b> package developpers team : <br>
-                    <i>\"Chiquet J, Donnet S, Barbillon P (2023). sbm: Stochastic Blockmodels. R package version 0.4.5, <a href = \"https://CRAN.R-project.org/package=sbm\">https://CRAN.R-project.org/package=sbm</a>.\"<i></li>
+                    <i>\"Chiquet J, Donnet S, Barbillon P (2023). sbm: Stochastic Blockmodels. R package version 0.4.5, <a href = \"https://CRAN.R-project.org/package=sbm\" target = \"_blank\">https://CRAN.R-project.org/package=sbm</a>.\"<i></li>
                     <li>Thanks the Migale bioinformatics facility : <br>
                     <i>\"We are grateful to the INRAE MIGALE bioinformatics facility (MIGALE, INRAE, 2020. Migale bioinformatics Facility, doi: 10.15454/1.5572390655343293E12) for providing help and/or computing and/or storage resources\"</i></li>
                   </ul>
@@ -36,13 +36,13 @@ mod_tab_about_us_ui <- function(id) {
 
                 <p>
                   <u>The demo dataset :</u> Vacher, Corinne, Dominique Piou, and Marie-Laure Desprez-Loustau.
-                  <a href = \"https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0001740\">
+                  <a href = \"https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0001740\" target = \"_blank\">
                   \"Architecture of an antagonistic tree/fungus network: the asymmetric influence of past evolutionary history.\"</a>
                   <i>PloS one</i> 3.3 (2008): e1740.
                 </p>
 
                 <p>
-                  <u>Developers:</u> <a href = \"https://mia-ps.inrae.fr/julie-aubert\"> Aubert Julie </a>  and  <a href = \"https://forgemia.inra.fr/theodore.vanrenterghem\"> Vanrenterghem Théodore </a>
+                  <u>Developers:</u> <a href = \"https://mia-ps.inrae.fr/julie-aubert\" target = \"_blank\"> Aubert Julie </a>  and  <a href = \"https://forgemia.inra.fr/theodore.vanrenterghem\" target = \"_blank\"> Vanrenterghem Theodore </a>
                   <br>
                   <p align=\"center\" position=\"absolute\" bottom\"80px\">
                     <img src=\"www/INRAE.png\" width=\"150\"/>
