8bade7e4630ec022821572780e58d9bfcd98d385
Theodore Vanrenterghem <theodore.vanrenterghem@inrae.fr>
2023-06-30 14:44:34 +0200

    add is.bipartite function to all files as open method

--- R/fct_is_bipartite.R
+++ R/fct_is_bipartite.R
@@ -2,11 +2,21 @@
 #'
 #' @description it's a function that says if your object (graph, sbm like or pre-sbm) is a bipartite one
 #'
-#' @param object is the object of wich you want to know the propreties
+#' @param object is a list or a result of {sbm} estimation that has to be checked whether or not is is bipartite
 #'
 #' @return TRUE if bipartite, FALSE if not
 #'
-#' @noRd
+#' @examples
+#' my_net1 <- list(pop = 8, networktype = "bipartite")
+#' is.bipartite(my_net1)
+#'
+#' my_net2 <- list(pop = 8, networktype = "unipartite")
+#' is.bipartite(my_net2)
+#'
+#' my_sbm <- sbm::estimateBipartiteSBM(sbm::fungusTreeNetwork$fungus_tree,model = 'bernoulli')
+#' is.bipartite(my_sbm)
+#'
+#' @export
 #'
 is.bipartite <- function(object) {
   UseMethod("is.bipartite", object)
@@ -17,11 +27,15 @@ is.bipartite <- function(object) {
 #'
 #' @description it's a function that says if your object (graph, sbm like or pre-sbm) is a bipartite one
 #'
-#' @param object is the object of wich you want to know the propreties
+#' @param object is a list that has to be checked whether or not is is bipartite
 #'
 #' @return TRUE if bipartite, FALSE if not
 #'
-#' @noRd
+#' @examples
+#'
+#' is.bipartite(1)
+#'
+#' @export
 #'
 is.bipartite.default <- function(object) {
   warning("object should be of class: 'list', 'SBM' or 'sbmMatrix'")
@@ -33,11 +47,19 @@ is.bipartite.default <- function(object) {
 #'
 #' @description it's a function that says if your object (graph, sbm like or pre-sbm) is a bipartite one
 #'
-#' @param object is the object of wich you want to know the propreties
+#' @param object is a list that has to be checked whether or not is is bipartite
 #'
 #' @return TRUE if bipartite, FALSE if not
 #'
-#' @noRd
+#' @examples
+#'
+#' my_net1 <- list(pop = 8, networktype = "bipartite")
+#' is.bipartite(my_net1)
+#'
+#' my_net2 <- list(pop = 8, networktype = "unipartite")
+#' is.bipartite(my_net2)
+#'
+#' @export
 #'
 is.bipartite.list <- function(object) {
   any(purrr::map_lgl(object,~identical(.x,"bipartite")))
@@ -47,11 +69,17 @@ is.bipartite.list <- function(object) {
 #'
 #' @description it's a function that says if your sbm is bipartite or not
 #'
-#' @param sbm an sbm model from {sbm} package
+#' @param object an sbm model from {sbm} package that has to be checked whether or not is is bipartite
 #'
 #' @return a Boolean, TRUE for bipartite or FALSE if else
 #'
-#' @noRd
+#' @examples
+#'
+#' my_sbm <- sbm::estimateBipartiteSBM(sbm::fungusTreeNetwork$fungus_tree,model = 'bernoulli')
+#' is.bipartite(my_sbm)
+#'
+#' @export
+#'
 is.bipartite.SBM <- function(object){
   if("BipartiteSBM" %in% class(object)){
     return(TRUE)
@@ -64,7 +92,7 @@ is.bipartite.SBM <- function(object){
 #'
 #' @description it's a function that says if your sbm is bipartite or not
 #'
-#' @param sbm an sbm model from {sbm} package
+#' @param object an sbmMatrix  that has to be checked whether or not is is bipartite
 #'
 #' @return a Boolean, TRUE for bipartite or FALSE if else
 #'
