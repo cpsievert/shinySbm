1edf50aa30f54ef75f6775c7194eb97667aeb7df
Jo-Theo <theodorevanrenterghem@gmail.com>
2023-07-12 22:27:20 +0200

    add sbm options

--- R/mod_tab_sbm.R
+++ R/mod_tab_sbm.R
@@ -23,7 +23,7 @@ mod_tab_sbm_ui <- function(id) {
             "Expected distribution &nbsp;",
             as.character(
               actionLink(ns("showLaws"),
-                         label = icon("circle-question")
+                label = icon("circle-question")
               )
             )
           ),
@@ -39,24 +39,48 @@ mod_tab_sbm_ui <- function(id) {
           conditionalPanel(
             condition = 'input.whichLaw == "bernoulli"', ns = ns,
             tags$div(
-              tags$strong("Bernoulli distribution:"),tags$br(),
-              " - It's a discrete probability distribution of a random variable which takes the value ",tags$strong("1")," with probability ",tags$strong("p")," and the value ",tags$strong("0")," with probability ",tags$strong("(1-p)"),".",tags$br(),
-              " - Less formally, the connection between two nodes can only be 0 (not-connected) or 1 (connected).",tags$br()
-            )),
+              tags$strong("Bernoulli distribution:"), tags$br(),
+              " - It's a discrete probability distribution of a random variable which takes the value ", tags$strong("1"), " with probability ", tags$strong("p"), " and the value ", tags$strong("0"), " with probability ", tags$strong("(1-p)"), ".", tags$br(),
+              " - Less formally, the connection between two nodes can only be 0 (not-connected) or 1 (connected).", tags$br(), tags$br()
+            )
+          ),
           conditionalPanel(
             condition = 'input.whichLaw == "poisson"', ns = ns,
             tags$div(
-              tags$strong("Poisson distribution:"),tags$br(),
-              " - Poisson distribution is a discrete probability distribution that expresses the probability of a given ",tags$strong("number of events")," occurring in a fixed interval of time or space.",tags$br(),
-              " - Less formally, the connection between two nodes can ",tags$strong("only be integers")," (like count data).",tags$br()
-            )),
+              tags$strong("Poisson distribution:"), tags$br(),
+              " - Poisson distribution is a discrete probability distribution that expresses the probability of a given ", tags$strong("number of events"), " occurring in a fixed interval of time or space.", tags$br(),
+              " - Less formally, the connection between two nodes can ", tags$strong("only be integers"), " (like count data).", tags$br(), tags$br()
+            )
+          ),
           conditionalPanel(
             condition = 'input.whichLaw == "gaussian"', ns = ns,
             tags$div(
-              tags$strong("Gaussian distribution:"),tags$br(),
-              " - The normal distribution or Gaussian distribution is a type of continuous probability distribution for a ",tags$strong("real-valued")," random variable.",tags$br(),
-              " - Less formally, the connection between nodes can be ",tags$strong("continuous measurements"),".",tags$br()
-            ))
+              tags$strong("Gaussian distribution:"), tags$br(),
+              " - The normal distribution or Gaussian distribution is a type of continuous probability distribution for a ", tags$strong("real-valued"), " random variable.", tags$br(),
+              " - Less formally, the connection between nodes can be ", tags$strong("continuous measurements"), ".", tags$br(), tags$br()
+            )
+          )
+        ),
+        actionLink(ns("moreSettings"),
+          label = HTML('<strong style = "color: black;">More settings</strong>'),
+          icon = icon("caret-square-down",style = 'color: black;')
+        ),
+        conditionalPanel(
+          condition = "input.moreSettings % 2  == 1", ns = ns,
+          numericInput(ns("exploreMin"),
+            label = "Explore at least (Blocks Number)",
+            value = 4,
+            min = 4,
+            max = 50,
+            step = 1
+          ),
+          numericInput(ns("exploreMax"),
+            label = "Stop exploration at (Block Number)",
+            value = NA,
+            min = 4,
+            max = 100,
+            step = 1
+          )
         ),
         uiOutput(ns("sbmButton"))
       ),
@@ -68,37 +92,37 @@ mod_tab_sbm_ui <- function(id) {
           title = "Download Tables", solidHeader = T,
           status = "info", width = 12,
           radioButtons(ns("whichTable"),
-                      "Select the Table",
-                      choices = list(
-                        "Block proportions" = "block_proportions",
-                        "Connectivity Table" = "connectivity_table",
-                        "Overall summary" = "stored_models"
-                      ),
-                      selected = "block_proportions"
+            "Select the Table",
+            choices = list(
+              "Block proportions" = "block_proportions",
+              "Connectivity Table" = "connectivity_table",
+              "Overall summary" = "stored_models"
+            ),
+            selected = "block_proportions"
           ),
           downloadButton(ns("downloadTable"),
-                         label = " Save Table",
-                         icon = icon("download", lib = "font-awesome")
+            label = " Save Table",
+            icon = icon("download", lib = "font-awesome")
           )
         )
       )
     ),
     column(
       width = 9,
-        shinydashboard::box(
-          title = "SBM outputs", solidHeader = T,
-          status = "info", width = 12,
-          strong("SBM code:"),
-          verbatimTextOutput(ns("sbmCode")),
-          mod_help_to_import_ui(ns("error_2")),
-          conditionalPanel(
-            condition = "output.sbmRan == 'YES'",
-            ns = ns_tab_upload,
-            hr(),
-            strong("SBM summary:"),
-            uiOutput(ns("ftsummary"))
-          )
+      shinydashboard::box(
+        title = "SBM outputs", solidHeader = T,
+        status = "info", width = 12,
+        strong("SBM code:"),
+        verbatimTextOutput(ns("sbmCode")),
+        mod_help_to_import_ui(ns("error_2")),
+        conditionalPanel(
+          condition = "output.sbmRan == 'YES'",
+          ns = ns_tab_upload,
+          hr(),
+          strong("SBM summary:"),
+          uiOutput(ns("ftsummary"))
         )
+      )
     )
   )
 }
@@ -109,6 +133,40 @@ mod_tab_sbm_ui <- function(id) {
 mod_tab_sbm_server <- function(id, r, parent_session) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
+
+    observeEvent(input$moreSettings,{
+      if (input$moreSettings %% 2 == 1) {
+        updateActionLink(session, "moreSettings",
+                         label = HTML('<strong style = "color: black;">Default settings</strong>'),
+                         icon = icon("caret-square-up",style = 'color: black;'))
+      } else {
+        updateActionLink(session, "moreSettings",
+                         label = HTML('<strong style = "color: black;">More settings</strong>'),
+                         icon = icon("caret-square-down",style = 'color: black;'))
+        updateNumericInput(session, "exploreMax", value = NA)
+        updateNumericInput(session, "exploreMin", value = 4)
+      }
+    })
+
+    observeEvent(input$exploreMin,{
+        updateNumericInput(session, "exploreMax", min = input$exploreMin+1)
+    })
+
+    exploreMax <- eventReactive(c(input$moreSettings,input$exploreMax),{
+      if(input$moreSettings %% 2 == 1){
+        if(is.na(input$exploreMax)){
+          Inf
+        }else{
+          input$exploreMax
+        }
+      }else{
+        Inf
+      }
+    })
+
+
+
+
 
     output$sbmButton <- renderUI({
       if (!is.null(r$upload$Dataset())) {
@@ -131,23 +189,25 @@ mod_tab_sbm_server <- function(id, r, parent_session) {
 
     output$sbmCode <- renderText({
       importSbm <- switch(r$upload$networkType(),
-                          "unipartite" = paste0(
-                            "mySbmModel <- sbm::estimateSimpleSBM(netMat = myNetworkMatrix, model = ",
-                            Dataset()$law, ", estimOptions = list(verbosity = 1))"
-                          ),
-                          "bipartite" = paste0(
-                            "mySbmModel <- sbm::estimateBipartiteSBM(netMat = myNetworkMatrix, model = '",
-                            Dataset()$law, "', estimOptions = list(verbosity = 1))"
-                          )
+        "unipartite" = paste0(
+          "mySbmModel <- sbm::estimateSimpleSBM(netMat = myNetworkMatrix, model = ",
+          Dataset()$law, ", estimOptions = list(verbosity = 1))"
+        ),
+        "bipartite" = paste0(
+          "mySbmModel <- sbm::estimateBipartiteSBM(netMat = myNetworkMatrix, model = '",
+          Dataset()$law, "', estimOptions = list(verbosity = 1))"
+        )
       )
-      if(sum(my_sbm_main()$nbBlocks) != sum(my_sbm()$nbBlocks)){
-        changeGroup <- paste0("\nindex <- which(mySbmModel$storedModels['nbBlocks'] == ",
-                              sum(my_sbm()$nbBlocks),")\n",
-                              "mySbmModel$setModel(index)")
-      }else{
-        changeGroup <- ''
+      if (sum(my_sbm_main()$nbBlocks) != sum(my_sbm()$nbBlocks)) {
+        changeGroup <- paste0(
+          "\nindex <- which(mySbmModel$storedModels['nbBlocks'] == ",
+          sum(my_sbm()$nbBlocks), ")\n",
+          "mySbmModel$setModel(index)"
+        )
+      } else {
+        changeGroup <- ""
       }
-      paste0(importSbm,changeGroup)
+      paste0(importSbm, changeGroup)
     })
 
     mod_help_to_import_server("error_2", sbmData = Dataset)
@@ -156,15 +216,24 @@ mod_tab_sbm_server <- function(id, r, parent_session) {
       session$userData$vars$sbm$runSbm <- input$runSbm
       data_res <- withProgress(message = "SBM is Running", {
         switch(r$upload$networkType(),
-               "unipartite" = sbm::estimateSimpleSBM(
-                 netMat = as.matrix(Dataset()),
-                 directed = r$upload$directed(),
-                 model = Dataset()$law, estimOptions = list(verbosity = 3, plot = F)
-               ),
-               "bipartite" = sbm::estimateBipartiteSBM(
-                 netMat = as.matrix(Dataset()),
-                 model = Dataset()$law, estimOptions = list(verbosity = 3, plot = F)
-               )
+          "unipartite" = sbm::estimateSimpleSBM(
+            netMat = as.matrix(Dataset()),
+            directed = r$upload$directed(),
+            model = Dataset()$law, estimOptions = list(
+              verbosity = 3,
+              plot = F,
+              exploreMin = input$exploreMin,
+              exploreMax = exploreMax()
+            )
+          ),
+          "bipartite" = sbm::estimateBipartiteSBM(
+            netMat = as.matrix(Dataset()),
+            model = Dataset()$law, estimOptions = list(
+              verbosity = 3, plot = F,
+              exploreMin = input$exploreMin,
+              exploreMax = exploreMax()
+            )
+          )
         )
       })
       shinyalert::shinyalert(
@@ -182,21 +251,21 @@ mod_tab_sbm_server <- function(id, r, parent_session) {
 
     observeEvent(my_sbm_main(), {
       updateRadioButtons(parent_session, "tab_show_1-whichMatrix",
-                         "Select Ploted Matrix",
-                         choices = list(
-                           "Raw Matrix" = "raw",
-                           "Reordered Matrix" = "ordered",
-                           "Expected Matrix" = "expected"
-                         ),
-                         selected = "ordered"
+        "Select Ploted Matrix",
+        choices = list(
+          "Raw Matrix" = "raw",
+          "Reordered Matrix" = "ordered",
+          "Expected Matrix" = "expected"
+        ),
+        selected = "ordered"
       )
       updateRadioButtons(parent_session, "tab_network_1-whichNetwork",
-                         "Select Ploted Network",
-                         choices = list(
-                           "Raw Network" = "raw",
-                           "Grouped Network" = "grouped"
-                         ),
-                         selected = "grouped"
+        "Select Ploted Network",
+        choices = list(
+          "Raw Network" = "raw",
+          "Grouped Network" = "grouped"
+        ),
+        selected = "grouped"
       )
     })
 
@@ -208,71 +277,83 @@ mod_tab_sbm_server <- function(id, r, parent_session) {
     )
 
 
-    observeEvent(c(my_sbm(),r$upload$labels()), {
+    observeEvent(c(my_sbm(), r$upload$labels()), {
       data_sbm <- my_sbm()$clone()
 
       ## Exemples values
-      first_par <- paste0("Tables 1 & 2 are the block description for the selected SBM. This model has an Entropy of ",
-                          round(data_sbm$entropy,2),
-                          ". The higher is the entropy, the less there is confusion inblock attribution.")
+      first_par <- paste0(
+        "Tables 1 & 2 are the block description for the selected SBM. This model has an Entropy of ",
+        round(data_sbm$entropy, 2),
+        ". The higher is the entropy, the less there is confusion inblock attribution."
+      )
 
       example_lab <- r$upload$labels()[["row"]]
-      example_connect <- round(data_sbm$connectParam$mean[1,1],2)
+      example_connect <- round(data_sbm$connectParam$mean[1, 1], 2)
       law <- stringr::str_to_title(data_sbm$modelName)
-      if(law == "Gaussian"){
-        example_param <- paste0("(mu = ",example_connect,", sigma","\uB2"," = ",round(data_sbm$connectParam$var,2),").")
-      }else if(law == "Poisson"){
-        example_param <- paste0("(lambda = ",example_connect,").")
-      }else{
-        example_param <- paste0("(p = ",example_connect,").")
+      if (law == "Gaussian") {
+        example_param <- paste0("(mu = ", example_connect, ", sigma", "\uB2", " = ", round(data_sbm$connectParam$var, 2), ").")
+      } else if (law == "Poisson") {
+        example_param <- paste0("(lambda = ", example_connect, ").")
+      } else {
+        example_param <- paste0("(p = ", example_connect, ").")
       }
 
-      if(is.bipartite(data_sbm)){
-        example_prop <- round_proportion(data_sbm$blockProp$row,2)[[1]]
-        connect_sentance <- paste0('a ',r$upload$labels()[["row"]], ' in block 1 and a ',r$upload$labels()[["col"]],' in block 1')
-      }else{
-        example_prop <- round_proportion(data_sbm$blockProp,2)[[1]]
-        connect_sentance <- 'two nodes in block 1'
+      if (is.bipartite(data_sbm)) {
+        example_prop <- round_proportion(data_sbm$blockProp$row, 2)[[1]]
+        connect_sentance <- paste0("a ", r$upload$labels()[["row"]], " in block 1 and a ", r$upload$labels()[["col"]], " in block 1")
+      } else {
+        example_prop <- round_proportion(data_sbm$blockProp, 2)[[1]]
+        connect_sentance <- "two nodes in block 1"
       }
 
-      second_par <- paste0("Table 1 gives blocks proportions, it's an indication of relative block sizes. If you take a random ",
-                           example_lab,", it has a probability of ",
-                           example_prop, " to belong to the first ",example_lab,
-                           " group.")
-      third_par <- paste0("Table 2 gives the connectivity values, it's an indication of block connection. Each values is a parameter of a ",
-                          law ," law. We can simulate the connection between ",
-                          connect_sentance," with a ",law ," law ",example_param)
+      second_par <- paste0(
+        "Table 1 gives blocks proportions, it's an indication of relative block sizes. If you take a random ",
+        example_lab, ", it has a probability of ",
+        example_prop, " to belong to the first ", example_lab,
+        " group."
+      )
+      third_par <- paste0(
+        "Table 2 gives the connectivity values, it's an indication of block connection. Each values is a parameter of a ",
+        law, " law. We can simulate the connection between ",
+        connect_sentance, " with a ", law, " law ", example_param
+      )
 
 
 
 
       output$ftsummary <- renderUI({
         tagList(
-          tags$div(tags$br(),
-                   first_par,
-                   second_par,
-                   third_par,
-                   tags$br(),
-                   tags$br()
+          tags$div(
+            tags$br(),
+            first_par,
+            second_par,
+            third_par,
+            tags$br(),
+            tags$br()
           ),
           fluidRow(
-            column(4,
-                   flexBlockProp(data_sbm,r$upload$labels()) %>%
-                     flextable::htmltools_value()),
-            column(8,
-                   flexConnect(data_sbm,r$upload$labels()) %>%
-                     flextable::htmltools_value())
+            column(
+              4,
+              flexBlockProp(data_sbm, r$upload$labels()) %>%
+                flextable::htmltools_value()
+            ),
+            column(
+              8,
+              flexConnect(data_sbm, r$upload$labels()) %>%
+                flextable::htmltools_value()
+            )
           ),
-          tags$div(tags$br(),
-                   "All stored models are in Table 3, the",
-                   HTML("<font color=red>red line</font>"),
-                   "is the best model based on ICL criteria. The",
-                   HTML('<mark color=orange>orange line</mark>'),
-                   "is the selected model.",
-                   tags$br(),
-                   tags$br()
+          tags$div(
+            tags$br(),
+            "All stored models are in Table 3, the",
+            HTML("<font color=red>red line</font>"),
+            "is the best model based on ICL criteria. The",
+            HTML("<mark color=orange>orange line</mark>"),
+            "is the selected model.",
+            tags$br(),
+            tags$br()
           ),
-          flexStoredModels(data_sbm,r$upload$labels()) %>%
+          flexStoredModels(data_sbm, r$upload$labels()) %>%
             flextable::htmltools_value()
         )
       })
@@ -280,20 +361,20 @@ mod_tab_sbm_server <- function(id, r, parent_session) {
 
 
     output$downloadTable <- downloadHandler(
-      filename = eventReactive(c(my_sbm(),input$whichTable),{
-        add_group <- paste0('_',sum(my_sbm()$nbBlocks),'_blocks')
-        return(paste0(input$whichTable,add_group,'.png'))
+      filename = eventReactive(c(my_sbm(), input$whichTable), {
+        add_group <- paste0("_", sum(my_sbm()$nbBlocks), "_blocks")
+        return(paste0(input$whichTable, add_group, ".png"))
       }),
-      content = function(file){
+      content = function(file) {
         data_sbm <- my_sbm()$clone()
-        if(input$whichTable == "block_proportions"){
-          ft <- flexBlockProp(data_sbm,r$upload$labels())
-        }else if(input$whichTable == "connectivity_table"){
-          ft <- flexConnect(data_sbm,r$upload$labels())
-        }else{
-          ft <- flexStoredModels(data_sbm,r$upload$labels())
+        if (input$whichTable == "block_proportions") {
+          ft <- flexBlockProp(data_sbm, r$upload$labels())
+        } else if (input$whichTable == "connectivity_table") {
+          ft <- flexConnect(data_sbm, r$upload$labels())
+        } else {
+          ft <- flexStoredModels(data_sbm, r$upload$labels())
         }
-        flextable::save_as_image(ft,path = file)
+        flextable::save_as_image(ft, path = file)
       }
     )
 
