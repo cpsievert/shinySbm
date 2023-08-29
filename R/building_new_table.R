1cf7158ebc4eb58977a284ab69b26e4d6908909e
Theodore Vanrenterghem <theodore.vanrenterghem@inrae.fr>
2023-08-29 11:05:38 +0200

    ok

--- R/building_new_table.R
+++ R/building_new_table.R
@@ -1,39 +1,100 @@
-library(shinySbm)
-myNetworkMatrix <- fungusTreeNetwork$fungus_tree %>%
-  `colnames<-`(fungusTreeNetwork$tree_names) %>%
-  `rownames<-`(fungusTreeNetwork$fungus_names)
 
-sbmMat <- fungusTreeNetwork$fungus_tree %>%
-  `colnames<-`(fungusTreeNetwork$tree_names) %>%
-  `rownames<-`(fungusTreeNetwork$fungus_names) %>%
-  as.data.frame() %>%
-  shinySbm:::buildSbmMatrix()
+get_datatable <- function(model,matrix, type = c('raw','ordered','predicted')){
+  UseMethod("get_datatable",model)
+}
 
-mySbmModel <- estimateBipartiteSBM(netMat = sbmMat$matrix, model = 'bernoulli',
-                                   estimOptions = list(plot = T, verbosity = 3))
-
+get_datatable.default <- function(model,matrix, type = c('raw','ordered','predicted')){
+  return(NULL)
+}
 
 ### still need to make table and build them as dataframe as doing this for simple sbm
-get_datatable.BipartiteSBM <- function(model,sbmMat, type = c('raw','ordered','predicted')){
+get_datatable.BipartiteSBM <- function(model,matrix, type = c('raw','ordered','predicted')){
   if(type[[1]] == 'raw'){
-    return(sbmMat$matrix)
+    return(shinySbm:::as.data.frame.sbmMatrix(matrix))
   }else{
-    clustering <- setNames(mySbmModel$memberships, c("row", "col"))
-    oRow <- order(clustering$row, decreasing = T)
-    oCol <- order(clustering$col, decreasing = F)
+    clustering <- setNames(model$memberships, c("row", "col"))
+    oRow <- order(clustering$row, decreasing = TRUE)
+    oCol <- order(clustering$col)
     uRow <- cumsum(table(clustering$row)) + 0.5
     uCol <- cumsum(table(clustering$col)) + 0.5
     if(type[[1]] == 'ordered'){
-      return(mySbmModel$networkData[oRow, oCol])
+      oMat <- model$networkData[oRow, oCol] %>%
+        `colnames<-`(matrix$nodes_names$col[oCol]) %>%
+        `rownames<-`(matrix$nodes_names$row[oRow]) %>%
+        as.data.frame()
+      return(oMat)
     }else{
-      return(mySbmModel$connectParam$mean[clustering$row, clustering$col][oRow, oCol])
+      pMat <- model$connectParam$mean[clustering$row, clustering$col][oRow, oCol] %>%
+        `colnames<-`(matrix$nodes_names$col[oCol]) %>%
+        `rownames<-`(matrix$nodes_names$row[oRow]) %>%
+        as.data.frame()
+      return(pMat)
+    }
+  }
+}
+
+
+get_datatable.SimpleSBM <- function(model,matrix, type = c('raw','ordered','predicted')){
+  if(type[[1]] == 'raw'){
+    return(shinySbm:::as.data.frame.sbmMatrix(matrix))
+  }else{
+    clustering <- list(row = model$memberships, col = model$memberships)
+    if (ordered) {
+      oRow <- order(clustering$row)
+      uRow <- cumsum(table(clustering$row)) + 0.5
+      uCol <- uRow[-length(uRow)]
+      uRow <- uRow[length(uRow)] - uRow[-length(uRow)] + 0.5
+      nb_rows <- dim(model$networkData)[1]
+      if(type[[1]] == 'ordered'){
+        oMat <- model$networkData[oRow, oRow][nb_rows:1, ] %>%
+          `colnames<-`(matrix$nodes_names[oRow]) %>%
+          `rownames<-`(matrix$nodes_names[oRow][nb_rows:1]) %>%
+          as.data.frame()
+        return(oMat)
+      }else{
+        pMat <-  model$connectParam$mean[clustering$row, clustering$col][oRow, oRow][nb_rows:1, ] %>%
+          `colnames<-`(matrix$nodes_names[oRow]) %>%
+          `rownames<-`(matrix$nodes_names[oRow][nb_rows:1]) %>%
+          as.data.frame()
+        return(pMat)
+      }
     }
   }
 }
 
 
 
+library(shinySbm)
 
+binetmat <- fungusTreeNetwork$fungus_tree %>%
+  `colnames<-`(fungusTreeNetwork$tree_names) %>%
+  `rownames<-`(fungusTreeNetwork$fungus_names)
+
+bisbmmat <- binetmat %>%
+  as.data.frame() %>%
+  shinySbm:::buildSbmMatrix()
+
+bisbm <- estimateBipartiteSBM(netMat = bisbmmat$matrix, model = 'bernoulli',
+                              estimOptions = list(plot = T, verbosity = 3))
+
+
+
+
+uninetmat <- fungusTreeNetwork$tree_tree %>%
+  `colnames<-`(fungusTreeNetwork$tree_names) %>%
+  `rownames<-`(fungusTreeNetwork$tree_names)
+
+unisbmmat <- uninetmat %>%
+  as.data.frame() %>%
+  shinySbm:::buildSbmMatrix()
+
+unisbm <- estimateBipartiteSBM(netMat = unisbmmat$matrix, model = 'poisson',
+                               estimOptions = list(plot = T, verbosity = 3))
+
+
+get_datatable(bisbm,bisbmmat, type = c('raw','ordered','predicted')[3]) %>% View
+
+get_datatable(unisbm,unisbmmat, type = c('raw','ordered','predicted')[3]) %>% View
 
 
 
